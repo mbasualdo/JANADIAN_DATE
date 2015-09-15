f8c9e89294831b51712725aaf12d321cbb8fa0f9
mbasualdo <mbasualdo@despegar.com>
2015-09-15 16:20:12 -0300

    creates

--- data/CREATES.sql
+++ data/CREATES.sql
@@ -4,12 +4,31 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-/** Creacion de tabla funcionalidad ***/
+
+/****** Funcion que chequea si un rol esta habilitado  ********/
+CREATE FUNCTION  [JANADIAN_DATE].[Rol_Habilitado](@id int)
+RETURNS  bit
+AS 
+BEGIN
+   DECLARE @retval bit
+   SELECT @retval = Habilitado FROM [JANADIAN_DATE].[Rol] WHERE id=@id
+   RETURN @retval
+END
+GO
+
+/** Creacion de tabla funcionalidad 
+IF OBJECT_ID( '[JANADIAN_DATE].[Funcionalidad]') IS NOT NULL
+DROP TABLE  [JANADIAN_DATE].[Funcionalidad];
+GO
+ ***/
+ /** Creacion de tabla funcionalidad  ***/
 CREATE TABLE [JANADIAN_DATE].[Funcionalidad](
 	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
 	[Descripcion] [nvarchar](255) NOT NULL
 
 ) ON [PRIMARY]
+
+GO
 /** Creacion de tabla rol ***/
 CREATE TABLE [JANADIAN_DATE].[Rol](
 	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
@@ -18,6 +37,8 @@ CREATE TABLE [JANADIAN_DATE].[Rol](
 	[Habilitado] [bit] NOT NULL DEFAULT 1 
 
 ) ON [PRIMARY]
+
+GO
 /** Creacion de tabla rol_funcionalidad ***/
 CREATE TABLE [JANADIAN_DATE].[Rol_Funcionalidad](
 	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
@@ -32,15 +53,6 @@ CREATE TABLE [JANADIAN_DATE].[Rol_Funcionalidad](
 	CONSTRAINT AK_Rol_Func UNIQUE (Rol,Funcionalidad)
 ) ON [PRIMARY]
 
-/****** Funcion que chequea si un rol esta habilitado  ********/
-CREATE FUNCTION  [JANADIAN_DATE].CheckFnctn(@id int)
-RETURNS  bit
-AS 
-BEGIN
-   DECLARE @retval bit
-   SELECT @retval = Habilitado FROM [JANADIAN_DATE].[Rol] WHERE id=@id
-   RETURN @retval
-END;
 GO
 
 /** Creacion de tabla usuario ***/
@@ -50,32 +62,28 @@ CREATE TABLE [JANADIAN_DATE].[Usuario](
 	[Password] [nvarchar](255) NOT NULL,
 	[Intentos] [int] NOT NULL DEFAULT 0,
 	[Rol] [int] NOT NULL,
+		/**Por defecto habilitado ***/
+	[Habilitado] [bit] NOT NULL DEFAULT 1 
 	CONSTRAINT FK_Usuario_Rol FOREIGN KEY (Rol) REFERENCES [JANADIAN_DATE].[Rol] (Id)
 	ON DELETE NO ACTION
     ON UPDATE CASCADE,
-	CONSTRAINT CHK_Rol_Habilitado CHECK ( [JANADIAN_DATE].CheckFnctn(Id)=1)
+	CONSTRAINT CHK_Rol_Habilitado CHECK ( [JANADIAN_DATE].[Rol_Habilitado](Id)=1)
 ) ON [PRIMARY]
 
+GO
+
 /** Creacion de trigger para deshabilitar usuario cuando tuvo 3 intentos ***/
-CREATE TRIGGER trgDeshabilitarUserFallido ON [JANADIAN_DATE].[Usuario]
+CREATE TRIGGER trgDeshabilitarUserFallido ON  [JANADIAN_DATE].[Usuario]
 FOR UPDATE
 AS
-	declare @intentos int = ;
-	declare @empname varchar(100);
-	declare @empsal decimal(10,2);
-	declare @audit_action varchar(100);
+BEGIN
+	declare @intentos int  ;
+	declare @id int;
 
-	select @empid=i.Emp_ID from inserted i;	
-	select @empname=i.Emp_Name from inserted i;	
-	select @empsal=i.Emp_Sal from inserted i;	
+	select @id=i.Id,@intentos=i.Intentos from inserted i;	
 	
-	if update(Emp_Name)
-		set @audit_action='Updated Record -- After Update Trigger.';
-	if update(Emp_Sal)
-		set @audit_action='Updated Record -- After Update Trigger.';
-
-	insert into Employee_Test_Audit(Emp_ID,Emp_Name,Emp_Sal,Audit_Action,Audit_Timestamp) 
-	values(@empid,@empname,@empsal,@audit_action,getdate());
-
-	PRINT 'AFTER UPDATE Trigger fired.'
+	if @intentos>=3
+		UPDATE [JANADIAN_DATE].[Usuario]  set Intentos = 0, Habilitado=0
+		where id=@id
+	end
 GO
