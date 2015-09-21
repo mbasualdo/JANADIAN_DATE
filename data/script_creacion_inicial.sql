a81b6e11af1ea2fecb1061bc3edc724fcccd0ffc
mbasualdo <mbasualdo@despegar.com>
2015-09-21 01:44:19 -0300

    script nd

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -23,6 +23,51 @@ BEGIN
    RETURN @retval
 END
 GO
+/****** Funcion que chequea si una aeronave esta habilitada  ********/
+CREATE FUNCTION  [JANADIAN_DATE].[Aeronave_Habilitada](@id int)
+RETURNS  bit
+AS 
+BEGIN
+   DECLARE @retval bit
+   SELECT @retval = Habilitado FROM [JANADIAN_DATE].[Aeronave] WHERE id=@id
+   RETURN @retval
+END
+GO
+/****** Funcion que devuelve el tipo de servicio de una aeronave  ********/
+CREATE FUNCTION  [JANADIAN_DATE].[Get_Tipo_Servicio_Aeronave](@id int)
+RETURNS  int
+AS 
+BEGIN
+   DECLARE @retval int
+   SELECT @retval = Tipo_Servicio FROM [JANADIAN_DATE].[Aeronave] WHERE id=@id
+   RETURN @retval
+END
+GO
+
+/****** Funcion que devuelve el tipo de servicio de una ruta  ********/
+CREATE FUNCTION  [JANADIAN_DATE].[Get_Tipo_Servicio_Ruta](@id int)
+RETURNS  int
+AS 
+BEGIN
+   DECLARE @retval int
+   SELECT @retval = Tipo_Servicio FROM [JANADIAN_DATE].[Ruta] WHERE id=@id
+   RETURN @retval
+END
+GO
+
+
+ /****** Funcion que devuelve cuantos viajes tiene en una fecha una aeronave  ********/
+CREATE FUNCTION  [JANADIAN_DATE].[Viajes_Fecha_Aeronave](@id int,@fecha datetime)
+RETURNS  int
+AS 
+BEGIN
+   DECLARE @count int
+   SELECT @count = COUNT(*) FROM [JANADIAN_DATE].[Viaje] WHERE Aeronave=@id AND DATEPART(yy,FechaSalida) = DATEPART(yy,@fecha) AND DATEPART(mm,FechaSalida) = DATEPART(mm,@fecha) AND DATEPART(dd,FechaSalida) = DATEPART(dd,@fecha)
+   RETURN  @count
+END
+GO
+
+
 
 /** Creacion de tabla funcionalidad 
 IF OBJECT_ID( '[JANADIAN_DATE].[Funcionalidad]') IS NOT NULL
@@ -151,11 +196,236 @@ CREATE TABLE [JANADIAN_DATE].[Ruta](
 	[Habilitado] [bit] NOT NULL DEFAULT 1 
 	CONSTRAINT FK_Ciudad_Origen FOREIGN KEY (Ciudad_Origen) REFERENCES [JANADIAN_DATE].[Ciudad] (Id)
 	ON DELETE NO ACTION
-    ON UPDATE CASCADE,
+    ON UPDATE  NO ACTION,
 	CONSTRAINT FK_Ciudad_Destino FOREIGN KEY (Ciudad_Destino) REFERENCES [JANADIAN_DATE].[Ciudad] (Id)
 	ON DELETE NO ACTION
-    ON UPDATE CASCADE,
+    ON UPDATE  NO ACTION,
 	CONSTRAINT CHK_Origen CHECK ( [Ciudad_Origen]<>[Ciudad_Destino])
+) ON [PRIMARY]
+
+GO
+
+/*** implementar el trigger que cancela pasajes y encomiendas al dar de baja una ruta (inhabilitar) ****/
+
+
+/***        ****/
+
+
+ /** Creacion de tabla aeronave  ***/
+CREATE TABLE [JANADIAN_DATE].[Aeronave](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Matricula] [nvarchar](255) NOT NULL UNIQUE,
+	[Modelo] [nvarchar](255) NOT NULL ,
+	[KG_Disponibles] [numeric](18,0) NOT NULL,
+	[Fabricante] [int] FOREIGN KEY REFERENCES  [JANADIAN_DATE].[Fabricante] (Id) NOT NULL,
+	[Tipo_Servicio] [int] FOREIGN KEY REFERENCES  [JANADIAN_DATE].[Tipo_Servicio] (Id) NOT NULL,
+	[Fecha_Alta] [datetime] NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		/**Por defecto sin bajas ***/
+	[Baja_Fuera_Servicio] [bit] NOT NULL DEFAULT 0,
+	[Baja_Vida_Util] [bit] NOT NULL DEFAULT 0,
+	[Fecha_Fuera_Servicio] [datetime],
+	[Fecha_Reinicio_Servicio] [datetime],
+	[Fecha_Baja_Definitiva] [datetime],
+	[Cant_Butacas_Ventanilla] [int]  NOT NULL,
+	[Cant_Butacas_Pasillo] [int]  NOT NULL,
+		/**Por defecto habilitado ***/
+	[Habilitado] [bit] NOT NULL DEFAULT 1
+
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de tabla butaca  ***/
+CREATE TABLE [JANADIAN_DATE].[Butaca](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Numero] [numeric](18,0) NOT NULL,
+	[Tipo] [nvarchar](255) NOT NULL,
+    [Piso] [numeric](18,0) NOT NULL,
+	[Aeronave] [int] NOT NULL,
+	CONSTRAINT FK_Butaca_Aeronave FOREIGN KEY (Aeronave) REFERENCES [JANADIAN_DATE].[Aeronave] (Id)
+	ON DELETE NO ACTION
+    ON UPDATE CASCADE
+
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de tabla viaje  ***/
+CREATE TABLE [JANADIAN_DATE].[Viaje](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[FechaSalida] [datetime] NOT NULL,
+	[Fecha_Llegada_Estimada] [datetime]  NOT NULL,
+	[FechaLlegada] [datetime],
+	[Aeronave] [int],
+	CONSTRAINT FK_Viaje_Aeronave FOREIGN KEY (Aeronave) REFERENCES [JANADIAN_DATE].[Aeronave] (Id)
+	ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+	[Ruta] [int],
+	CONSTRAINT FK_Ruta_Viaje FOREIGN KEY (Ruta) REFERENCES [JANADIAN_DATE].[Ruta] (Id)
+	ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+	CONSTRAINT CHK_Fechas_Futuras CHECK ( DATEDIFF(day,CURRENT_TIMESTAMP,[FechaSalida])>0 and DATEDIFF(day,CURRENT_TIMESTAMP,[Fecha_Llegada_Estimada])>0 and DATEDIFF(day,CURRENT_TIMESTAMP,[FechaLlegada])>0),
+	CONSTRAINT CHK_Mismo_Tipo_Servicio CHECK ([JANADIAN_DATE].[Get_Tipo_Servicio_Aeronave]([Aeronave]) = [JANADIAN_DATE].[Get_Tipo_Servicio_Ruta]([Ruta]) ),
+	CONSTRAINT CHK_Aeronave_Disponible CHECK ([JANADIAN_DATE].[Aeronave_Habilitada]([Aeronave])=1 AND [JANADIAN_DATE].[Viajes_Fecha_Aeronave]([Aeronave],[FechaSalida])=0)
+
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de vista itinerario aeronave ***/
+ CREATE VIEW [JANADIAN_DATE].[Itinerario_Aeronave] AS  
+ SELECT a.Matricula,v.FechaLlegada,o.Nombre as Origen,d.Nombre as Destino FROM   [JANADIAN_DATE].[Viaje] v
+ INNER JOIN [JANADIAN_DATE].[Aeronave] a ON (v.Aeronave = a.Id)
+ INNER JOIN [JANADIAN_DATE].[Ruta] r ON (v.Ruta = r.Id)
+ INNER JOIN [JANADIAN_DATE].[Ciudad] o ON (r.Ciudad_Origen = o.Id)
+ INNER JOIN [JANADIAN_DATE].[Ciudad] d ON (r.Ciudad_Destino = d.Id)
+
+ GO
+
+
+ /** Creacion de tabla Butaca viaje ***/
+CREATE TABLE [JANADIAN_DATE].[Butaca_Viaje](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Butaca] [int] NOT NULL,
+	[Viaje] [int] NOT NULL,
+	CONSTRAINT FK_Butaca FOREIGN KEY (Butaca) REFERENCES [JANADIAN_DATE].[Butaca] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+	CONSTRAINT FK_Viaje FOREIGN KEY (Viaje) REFERENCES [JANADIAN_DATE].[Viaje] (Id),
+	CONSTRAINT AK_Butaca_Viaje UNIQUE (Butaca,Viaje)
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de tabla Cliente ***/
+CREATE TABLE [JANADIAN_DATE].[Cliente](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Dni] [numeric](18,0) UNIQUE NOT NULL,
+	[Nombre] [nvarchar](255) NOT NULL,
+	[Apellido] [nvarchar](255) NOT NULL,
+	[Dir] [nvarchar](255) NOT NULL,
+	[Telefono] [numeric](18,0) UNIQUE NOT NULL,
+	[Mail] [nvarchar](255),
+	[Fecha_Nac] [datetime] NOT NULL,
+) ON [PRIMARY]
+
+GO
+
+
+ /** Creacion de tabla Compra ***/
+CREATE TABLE [JANADIAN_DATE].[Compra](
+	[PNR] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Precio] [numeric](18,2) NOT NULL,
+	[Fecha_Compra] [datetime] NOT NULL,
+	[Viaje] [int] NOT NULL,
+	[Forma_Pago] [nvarchar](255) NOT NULL,
+	CONSTRAINT FK_Compra_Viaje FOREIGN KEY (Viaje) REFERENCES [JANADIAN_DATE].[Viaje] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de vista viajes disponibles ***/
+ CREATE VIEW [JANADIAN_DATE].[Viaje_Disponible] AS  
+ SELECT v.FechaSalida,o.Nombre as Origen,d.Nombre as Destino FROM   [JANADIAN_DATE].[Viaje] v
+ INNER JOIN [JANADIAN_DATE].[Ruta] r ON (v.Ruta = r.Id)
+ INNER JOIN [JANADIAN_DATE].[Ciudad] o ON (r.Ciudad_Origen = o.Id)
+ INNER JOIN [JANADIAN_DATE].[Ciudad] d ON (r.Ciudad_Destino = d.Id)
+
+ GO
+
+  /** Creacion de tabla Pasaje ***/
+CREATE TABLE [JANADIAN_DATE].[Pasaje](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Codigo] [numeric](18,0) NOT NULL UNIQUE,
+	[Butaca] [int],
+	[KG] [numeric](18,0),
+	[Compra] [int] NOT NULL FOREIGN KEY (Compra) REFERENCES [JANADIAN_DATE].[Compra] (PNR),
+	[Cliente] [int] NOT NULL,
+	CONSTRAINT FK_Pasaje_Butaca FOREIGN KEY (Butaca) REFERENCES [JANADIAN_DATE].[Butaca] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+	CONSTRAINT FK_Pasaje_Cliente FOREIGN KEY (Cliente) REFERENCES [JANADIAN_DATE].[Cliente] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+) ON [PRIMARY]
+
+GO
+
+
+  /** Creacion de tabla Datos tarjeta ***/
+CREATE TABLE [JANADIAN_DATE].[Datos_Tarjeta](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Numero] [numeric](18,0) NOT NULL UNIQUE,
+	[Tipo] [nvarchar](255) NOT NULL,
+	[Cod_Seg]  [numeric](18,0) NOT NULL,
+	[Fecha_Venc] [nvarchar](4) NOT NULL,
+	[Compra] [int] NOT NULL FOREIGN KEY (Compra) REFERENCES [JANADIAN_DATE].[Compra] (PNR),
+	[Cliente] [int] NOT NULL,
+	CONSTRAINT FK_Tarjeta_Cliente FOREIGN KEY (Cliente) REFERENCES [JANADIAN_DATE].[Cliente] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de tabla Producto  ***/
+CREATE TABLE [JANADIAN_DATE].[Producto](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Nombre] [nvarchar](255) NOT NULL UNIQUE,
+	[Stock] [int] NOT NULL
+
+
+) ON [PRIMARY]
+
+GO
+
+
+ /** Creacion de tabla Canje  ***/
+CREATE TABLE [JANADIAN_DATE].[Canje](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Fecha] [datetime] NOT NULL,
+	[Cantidad] [int] NOT NULL,
+	[Cliente] [int] NOT NULL,
+	CONSTRAINT FK_Canje_Cliente FOREIGN KEY (Cliente) REFERENCES [JANADIAN_DATE].[Cliente] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+	[Producto] [int] NOT NULL,
+	CONSTRAINT FK_Canje_Producto FOREIGN KEY (Producto) REFERENCES [JANADIAN_DATE].[Producto] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de tabla Millas  ***/
+CREATE TABLE [JANADIAN_DATE].[Millas](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[Fecha] [datetime] NOT NULL,
+	[Cantidad] [int] NOT NULL,
+	[Cliente] [int] NOT NULL,
+	CONSTRAINT FK_Millas_Cliente FOREIGN KEY (Cliente) REFERENCES [JANADIAN_DATE].[Cliente] (Id)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+) ON [PRIMARY]
+
+GO
+
+ /** Creacion de tabla Cancelacion  ***/
+CREATE TABLE [JANADIAN_DATE].[Cancelacion](
+	[Id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[PNR] [int] NOT NULL,
+	[Codigo] [int] NOT NULL  FOREIGN KEY (Codigo) REFERENCES [JANADIAN_DATE].[Pasaje] (Id),
+	[Motivo] [nvarchar](255) NOT NULL,
+	[FechaDevolucion] [datetime] NOT NULL,
+	[TipoCompra] [nvarchar](255) NOT NULL,
+	CONSTRAINT FK_Cancelacion_Compra FOREIGN KEY (PNR) REFERENCES [JANADIAN_DATE].[Compra] (PNR)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE
 ) ON [PRIMARY]
 
 GO
