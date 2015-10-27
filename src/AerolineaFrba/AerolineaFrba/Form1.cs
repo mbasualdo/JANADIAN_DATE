7b0e6c874fb80187681f6fa634a68f3c25278196
mbasualdo <mbasualdo@despegar.com>
2015-10-27 11:52:59 -0300

    conection y primer query

--- src/AerolineaFrba/AerolineaFrba/Form1.cs
+++ src/AerolineaFrba/AerolineaFrba/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
+using System.Data.SqlClient;
+
 namespace AerolineaFrba
 {
     public partial class Form1 : Form
@@ -19,8 +21,34 @@ namespace AerolineaFrba
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string conStr = ConfigurationManager.ConnectionStrings["GD2C2015"].ConnectionString;
-            Console.Write(conStr);
+            string connectionString = ConfigurationManager.ConnectionStrings["GD2C2015"].ConnectionString;
+//            Console.Write(conStr);
+//            System.Data.SqlClient.SqlConnection cn = new System.Data.SqlClient.SqlConnection(conStr);
+//           cn.Open();
+//            cn.ToString();
+
+	//
+	// In a using statement, acquire the SqlConnection as a resource.
+	//
+	using (SqlConnection con = new SqlConnection(connectionString))
+	{
+	    //
+	    // Open the SqlConnection.
+	    //
+	    con.Open();
+	    //
+	    // The following code uses an SqlCommand based on the SqlConnection.
+	    //
+        using (SqlCommand command = new SqlCommand("SELECT TOP 2 * FROM [GD2C2015].[JANADIAN_DATE].[Compra]", con))
+	    using (SqlDataReader reader = command.ExecuteReader())
+	    {
+		while (reader.Read())
+		{
+		    Console.WriteLine("{0} {1} {2}",
+			reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+		}
+	}
+    }
         }
     }
 }
