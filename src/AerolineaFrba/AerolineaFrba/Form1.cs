78631142df6d828038fe7446d1035e746f14fece
mbasualdo <mbasualdo@despegar.com>
2015-10-27 16:27:47 -0300

    get reader

--- src/AerolineaFrba/AerolineaFrba/Form1.cs
+++ src/AerolineaFrba/AerolineaFrba/Form1.cs
@@ -21,34 +21,13 @@ namespace AerolineaFrba
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["GD2C2015"].ConnectionString;
-//            Console.Write(conStr);
-//            System.Data.SqlClient.SqlConnection cn = new System.Data.SqlClient.SqlConnection(conStr);
-//           cn.Open();
-//            cn.ToString();
 
 	//
 	// In a using statement, acquire the SqlConnection as a resource.
 	//
-	using (SqlConnection con = new SqlConnection(connectionString))
-	{
-	    //
-	    // Open the SqlConnection.
-	    //
-	    con.Open();
-	    //
-	    // The following code uses an SqlCommand based on the SqlConnection.
-	    //
-        using (SqlCommand command = new SqlCommand("SELECT TOP 2 * FROM [GD2C2015].[JANADIAN_DATE].[Compra]", con))
-	    using (SqlDataReader reader = command.ExecuteReader())
-	    {
-		while (reader.Read())
-		{
-		    Console.WriteLine("{0} {1} {2}",
-			reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-		}
-	}
-    }
+
+            int cod = JanadianDateDB.Instance.getCompras();
+            cod.ToString();
         }
     }
 }
---
+++ src/AerolineaFrba/AerolineaFrba/JanadianDateDB.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba
+{
+    public sealed class JanadianDateDB
+    {
+        private static readonly JanadianDateDB instance = new JanadianDateDB();
+        private readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["GD2C2015"].ConnectionString);
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static JanadianDateDB()
+        {
+        }
+
+        private JanadianDateDB()
+        {
+        }
+
+        public static JanadianDateDB Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public SqlConnection GetDBConnection()
+        {
+            return con;
+        }
+
+        public int getCompras() { 
+        using (SqlConnection con = JanadianDateDB.Instance.GetDBConnection())
+	{
+	    //
+	    // Open the SqlConnection.
+	    //
+	    con.Open();
+	    //
+	    // The following code uses an SqlCommand based on the SqlConnection.
+	    //
+        using (SqlCommand command = new SqlCommand("SELECT TOP 2 * FROM [GD2C2015].[JANADIAN_DATE].[Compra]", con))
+	    using (SqlDataReader reader = command.ExecuteReader())
+	    {
+		while (reader.Read())
+		{
+		 //   Console.WriteLine("{0} {1} {2}",
+      //      reader.GetString(0), reader.GetString(4), reader.GetString(6));
+
+            return reader.GetInt32(0);
+		}
+	}
+    }
+        return 0;
+        }
+    }
+}
